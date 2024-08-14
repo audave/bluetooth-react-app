1b57f64e705a77210703ab16e018027caeecd53a
David Coote <david.coote@adelaide.edu.au>
2024-08-14 16:32:40 +0930

    Early Commit Bluetooth app monitory battery level

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [device, setDevice] = useState(null);
+    const [gattServer, setGattServer] = useState(null);
+    const [deviceService, setDeviceService] = useState(null);
+    const [characteristic, setCharacteristic] = useState(null);
+
+    navigator.bluetooth.addEventListener('onserviceadded', function (event) {
+        console.log(event)
+    })
+
+    const handleButtonPush = async () => {
+        const device = await navigator.bluetooth
+            .requestDevice({
+                filters: [
+                    {services: ["battery_service"]}
+                ],
+                optionalServices: ['battery_service'],
+            })
+        setDevice(device)
+        const server = await device.gatt.connect()
+        setGattServer(server)
+        const service = await server.getPrimaryService('battery_service');
+        setDeviceService(service);
+        const characteristic = await service.getCharacteristic('battery_level')
+        setCharacteristic(characteristic);
+    }
+
+    return (
+        <div className="App">
+            <ConnectDeviceButton clickAction={handleButtonPush}/>
+            {(device !== null) ? <TextBox customText={device.name}/> : null}
+            {(characteristic !== null) ? <BlueToothDeviceCharateristic characteristic={characteristic}/> : null}
+        </div>
+    );
+}
+
+
+const ConnectDeviceButton = ({clickAction}) => {
+    return (<button
+        onClick={clickAction}
+    >Connect Device</button>)
+}
+
+const TextBox = ({customText}) => {
+    return (<p>Device: Name {customText}</p>)
+}
+
+const BlueToothDeviceCharateristic = ({characteristic}) => {
+    const [value, setValue] = useState("0")
+    const [count, setCount] = useState(0);
+
+    const test = (event) => {
+        console.log(event.timeStamp);
+        console.log(count);
+        console.log(event);
+        // setValue(event.target.value.getUint8(0));
+        // setCount(count +1)
+    }
+
+    useEffect(() => {
+        async function getCharactertisticValue() {
+            characteristic.readValue().then((readValue) => {
+                    setValue(readValue.getUint8(0));
+                }
+            );
+        }
+
+        getCharactertisticValue();
+    })
+    characteristic.addEventListener('characteristicvaluechanged', test);
+    return (<div><p>Battery Level is {value} %</p></div>)
 }
 
 export default App;
